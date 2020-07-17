643affb1d924d8d9281d10ed22484840c787e67f
Reilly Oduory <Home@TECH-SOARES.vault.wananchi.com>
2020-07-17 12:40:54 +0300

    Javascript finished

--- README.md
+++ README.md
@@ -1,12 +1,8 @@
 # Akhan-WA
 A webapp for generating names based on the Akhan System
-#Akhan naming system
+# Akhan naming system
 Its a system where children's first names were given according to the day of the week they were born
-#languages used
+# languages used
 html
 css
-<<<<<<< HEAD
 javascript(vanilla)
-=======
-javascript(vanilla)
->>>>>>> 3ff7258b1d522e71bce601e7f7f5b6348fbbb19c
---
+++ js/index.js
@@ -0,0 +1,83 @@
+//variables
+
+//names to be allocated
+//males
+var male = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+//females
+var female = ['Akosua', 'Adowa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+
+//arithmetic
+var dd = '';
+var mm = '';
+var cc = '';
+var yy = '';
+var gender = '';
+var date = document.getElementById('dateOfBirth');
+
+//submit function
+function submit() {
+    var fetch = date.value;
+    toString(fetch);
+    var strBreak = fetch.split('');
+
+    //var for day
+    var dOne = strBreak[8];
+    var dTwo = strBreak[9];
+    var siku = dOne.concat(dTwo);
+    toString(siku);
+    var dd = siku;
+    dd = parseInt(dd);
+    console.log(dd);
+    //siku is day in swahili
+
+    //var for month
+    var mOne = strBreak[5];
+    var mTwo = strBreak[6];
+    var mwezi = mOne.concat(mTwo);
+    toString(mwezi);
+    var mm = mwezi;
+    mm = parseInt(mm);
+    console.log(mm);
+    //mwezi is month in swahili
+
+    //var for century
+    var cOne = strBreak[0];
+    var cTwo = strBreak[1];
+    var karne = cOne.concat(cTwo);
+    toString(karne);
+    var cc = karne;
+    cc = parseInt(cc);
+    console.log(cc);
+    //karne is century in swahili
+
+    //var for year
+    var yOne = strBreak[2];
+    var yTwo = strBreak[3];
+    var mwaka = yOne.concat(yTwo);
+    toString(mwaka);
+    var yy = mwaka;
+    yy = parseInt(yy);
+    console.log(yy);
+    //mwaka is year in swahili
+
+    //computation
+    var dayOfTheWeek = Math.round((((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7);
+    console.log(dayOfTheWeek);
+
+    //gender
+    var gender = document.getElementsByName('gender').value;
+    // toString(gender);
+    //console.log(gender);
+    var name = '';
+    if (document.getElementById('male').checked) {
+        name = male[dayOfTheWeek - 1];
+    } else if (document.getElementById('female').checked) {
+        name = female[dayOfTheWeek - 1];
+    } else {
+        alert('Select Gender');
+        return;
+    }
+    document.getElementById('answer').innerText = name;
+    // alert(name);
+
+}
